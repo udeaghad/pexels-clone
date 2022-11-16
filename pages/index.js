d63cdc82b87db6aa8a9ef5bd79fb9a3c4c26280c
kingsley achumie <kingsonly13c@gmail.com>
2022-11-16 16:26:30 +0100

    fixed tailwind

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+      <div className="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
+   <p className="text-white font-extrabold text-3xl md:text-5xl">   Ghostwind CSS
+   </p>
+<p className="text-xl md:text-2xl text-gray-500"> Welcome to my Blog </p>
+</div>
+
+
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
