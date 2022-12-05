96d5c5c6ced74d67af147de584ace179338b3344
Joynas <Ibinichinasajoy@gmail.com>
2022-12-05 19:03:19 +0100

    logo-component-update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,7 @@ module.exports = {
         "./pages/**/*.{js,ts,jsx,tsx}",   
         "./components/**/*.{js,ts,jsx,tsx}",  
          ],
-<<<<<<< HEAD
-  theme: {  
-
-=======
   theme: {   
->>>>>>> fe8d785 (logo-component-updated)
          extend: {},
          },  
   plugins: [],
