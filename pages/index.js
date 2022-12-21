dc55499f963d11a3fe1238181d20ddb3b52f31d1
Joynas <Ibinichinasajoy@gmail.com>
2022-12-21 09:57:31 +0100

    wip

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,6 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Deploy &rarr;</h2>
-            
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
