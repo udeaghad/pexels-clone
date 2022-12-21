48fd181c85a99c921cf1360a2ad0dc61b1499287
Joynas <Ibinichinasajoy@gmail.com>
2022-12-21 09:39:22 +0100

    wip

--- components/menu-folder/menu.js
+++ components/menu-folder/menu.js
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import React from 'react';
 
 /**
  * Represents the menu dropdown container.
  * @function
  * @param {string} props - stores the value of the dropped down list.
- * @Description - this component works dynamically, it is both used to generate the hidden 
+ * @Description - this component works dynamically, it is both used to generate the hiden 
  * dropdown list both of "Explore" and
  * "Profile" on the menu
  */
@@ -12,12 +13,11 @@ import Link from "next/link";
 export default function MenuDropdown(props) {
     const { dropdown } = props;
     return (
-        //data test id is the id used to get a elements passed as arguments to the test files
-        <div className="bg-white w-48 h-[auto] rounded-[20px] py-5 leading-9 font-medium" data-testid="dropdown-list">
+        <div className="bg-white w-48 h-[auto] rounded-[20px] py-5 leading-9 font-medium" data-testid="no">
             <ul style={{ listStyle: "none" }}>
                 {dropdown.map((items, keys) => (
-                    <li className="hover:bg-slate-100 pl-6" key={items.id}>
-                        <Link href={items.cta} >{items.title}</Link>
+                    <li className="hover:bg-slate-100 pl-6" key={keys}>
+                        <Link href={items} >{items}</Link>
                     </li>
                 ))}
             </ul>
--- test/menu.test.js
+++ test/menu.test.js
@@ -1,41 +1,89 @@
 import MenuDropdown from "./../components/menu-folder/menu";
 import "@testing-library/jest-dom";
 import { fireEvent, render, screen } from "@testing-library/react";
+// import { shallow } from "enzyme"
+
 
 describe("dropdown list", () => {
-    it("renders a dropdown list", () => {
-      const menuList = [
-        "Discorver Photos",
-        "Popular Searches",
-        "Leaderboard",
-        "Challenges",
-        "Free Videos",
-        "Pexel Blog",
-      ];
-      render(<MenuDropdown dropdown={menuList}  />);
-      // check if all components are rendered
-      
-      
-      expect(screen.getByTestId("yes")).toBeInTheDocument();
-  
-    });
-  });
+  it("renders a dropdown list", () => {
+    const menuList = [
+      {
+        title: "Discorver Photos,",
+        cta: "/"
+      },
+      {
+        title: "Popular Searches,",
+        cta: "/"
+      },
+
+      {
+        title: "Leaderboard,",
+        cta: "/"
+      },
+
+      {
+        title: "Challenges,",
+        cta: "/"
+      },
+      {
+        title: "Free Videos,",
+        cta: "/"
+      },
+      {
+        title: "Pexel Blog,",
+        cta: "/"
+      }
+
+    ];
+    render(<MenuDropdown dropdown={menuList} />);
+    // check if all components are rendered
 
 
-  describe("dropdown list new", () => {
-    it("renders a dropdown list", () => {
-      const menuList = [
-        "Discorver Photos",
-        "Popular Searches",
-        "Leaderboard",
-        "Challenges",
-        "Free Videos",
-        "Pexel Blog",
-      ];
-      render(<MenuDropdown dropdown={menuList}  />);
-      // check if all components are rendered
-      
-      
-      expect(screen.getByTestId("yes")).toBeInTheDocument();
-    });
+    expect(screen.getByTestId("dropdown-list")).toHaveTextContent("Discorver Photos");
+    expect(screen.getByTestId("dropdown-list")).toHaveTextContent("Popular Searches");
+    expect(screen.getByTestId("dropdown-list")).toHaveTextContent("Leaderboard");
+    expect(screen.getByTestId("dropdown-list")).toHaveTextContent("Challenges");
+    expect(screen.getByTestId("dropdown-list")).toHaveTextContent("Free Videos");
+    expect(screen.getByTestId("dropdown-list")).toHaveTextContent("Pexel Blog");
+
   });
+});
+
+describe("dropdown list new", () => {
+  it("renders a dropdown list", () => {
+    const menuList = [
+      {
+        title: "Discorver Photos,",
+        cta: "/"
+      },
+      {
+        title: "Popular Searches,",
+        cta: "/"
+      },
+
+      {
+        title: "Leaderboard,",
+        cta: "/"
+      },
+
+      {
+        title: "Challenges,",
+        cta: "/"
+      },
+      {
+        title: "Free Videos,",
+        cta: "/"
+      },
+      {
+        title: "Pexel Blog,",
+        cta: "/"
+      }
+
+    ];
+    render(<MenuDropdown dropdown={menuList} />);
+    // check if all components are rendered
+
+
+    expect(screen.getByTestId("dropdown-list")).toBeInTheDocument();
+  });
+});
