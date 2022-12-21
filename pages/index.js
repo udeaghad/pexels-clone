07230b5d913d7232d0b101698cb9525056fe3a04
Joynas <Ibinichinasajoy@gmail.com>
2022-12-21 09:35:01 +0100

    wip

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,47 @@ export default function Home() {
       </Head>
         
       <main className={styles.main}>
-
+        <div className="container max-w-4xl mx-auto pt-16 md:pt-32 text-center break-normal">
+          <p className="text-white font-extrabold text-3xl md:text-5xl">   Ghostwind CSS
+          </p>
+          <p className="text-xl md:text-2xl text-[blue]"> Welcome to my Blog </p>
+        </div>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+        <div className="border-2 border-dotted border-[red]">
+    red
+        </div>
+        <div className={styles.grid}>
+          <a href="https://nextjs.org/docs" className={styles.card}>
+            <h2>Documentation &rarr;</h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+          <a href="https://nextjs.org/learn" className={styles.card}>
+            <h2>Learn &rarr;</h2>
+            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          </a>
+          <a
+            href="https://github.com/vercel/next.js/tree/canary/examples"
+            className={styles.card}
+          >
+            <h2>Examples &rarr;</h2>
+            <p>Discover and deploy boilerplate example Next.js projects.</p>
+          </a>
+          <a
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            className={styles.card}
+          >
+            <h2>Deploy &rarr;</h2>
+            <p>
+              Instantly deploy your Next.js site to a public URL with Vercel.
+            </p>
+          </a>
+        </div>
       </main>
       <footer className={styles.footer}>
         <a
