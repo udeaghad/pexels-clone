2beac45334d5682ea354ab6a6713f3d221431dd8
kingsley achumie <kingsonly13c@gmail.com>
2023-01-04 00:03:00 +0100

    resolve issue with rebase

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import { FaBeer } from 'react-icons/fa';
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 
-
 export default function Home() {
   return (
     <div className={styles.container} data-testid="result">
