e17bab25e3848b48d3e3b700f1acd3f0b19ef02f
kingsley achumie <kingsonly13c@gmail.com>
2023-01-03 23:53:24 +0100

    resolve issue with rebase

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-=======
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { FaBeer } from 'react-icons/fa';
-
->>>>>>> 2fe6c10 (wip)
 
 export default function Home() {
   return (
