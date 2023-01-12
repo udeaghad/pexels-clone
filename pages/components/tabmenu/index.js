e2b1b8c6bbdf293dadc80934c93153d89626943a
Joynas <Ibinichinasajoy@gmail.com>
2023-01-12 15:49:46 +0100

    created-tabmenu-bar

--- pages/components/tabmenu/index.js
+++ pages/components/tabmenu/index.js
@@ -1,20 +1,38 @@
 import React from "react";
+import Link from "next/link";
 import Caption from "../../../components/Caption/Caption";
 import Tabmenu from "../../../components/Tabmenu/Tabmenu";
-// import TabMenu from "../../../components/Tabmenu/Tabmenu";
-
 
 export default function TabMenuBar() {
+  const tablist = [
+    {
+      id: 1,
+      text: "Home",
+      link: "/home",
+    },
+    {
+      id: 2,
+      text: "Videos",
+      link: "/videos",
+    },
+    {
+      id: 3,
+      text: "Leaderboard",
+      link: "/leaderboard",
+    },
+    {
+      id: 4,
+      text: "Challenges",
+      link: "/challenges",
+    },
+  ];
   return (
-    <div>
-        gggghh
-        {/* <Tabmenu/> */}
-        <Caption text="yodondndnnndndnndnd" color="" size=""/>
-      <Tabmenu
-       link="/"
-       type="text" 
-       cta="" 
-       />
-    </div>
-  );
+  <div>
+    
+     <Tabmenu 
+  tablist={tablist}
+   />;
+  </div>
+  )
+ 
 }
