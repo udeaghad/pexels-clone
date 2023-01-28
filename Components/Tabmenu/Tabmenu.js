823ae6dbe827a9c1d0d46ab9713d16b2cc015777
kingsley achumie <kingsonly13c@gmail.com>
2023-01-28 14:57:44 +0100

    wip

--- Components/Tabmenu/Tabmenu.js
+++ Components/Tabmenu/Tabmenu.js
@@ -3,30 +3,31 @@ import Link from "next/link";
 import Router from "next/router";
 
 export default function Tabmenu(props) {
-  const { tablist, link } = props;
-  const [showTabMenu, setTabMenu] = useState();
+    const { tablist, link } = props;
+    const [showTabMenu, setTabMenu] = useState();
 
-  const activeTabMenu = () => {
-    setTabMenu(true);
-  };
+    const activeTabMenu = () => {
+        setTabMenu(true);
+    };
 
-  return (
-    <div>
-      <ul className="flex flex-row gap-4 text-slate-600 pt-6 ">
-        {tablist.map((tabbar, index) => (
-          <div
-            className={`h-12 flex justify-center items-center p-4 rounded-3xl 
-            ${Router && Router.pathname == tabbar.link ? "bg-black text-white " : ""}`}
-            key={tabbar.id}
-            onClick={() => {
-              setTabMenu(index);
-              Router.push(`/${tabbar.link}`);
-            }}
-          >
-            <div>{tabbar.text}</div>
-          </div>
-        ))}
-      </ul>
-    </div>
-  );
+    return (
+         <div>
+        <ul className = "flex flex-row gap-4 text-slate-600 pt-6 " > 
+        {
+            tablist.map((tabbar, index) => ( 
+                <div  className={Router.pathname == tabbar.link ? "bg-black text-white h-12 flex justify-center items-center p-4 rounded-3xl " : "h-12 flex justify-center items-center p-4 rounded-3xl"}
+                key = { tabbar.id }
+                onClick = {
+                    () => {
+                        setTabMenu(index);
+                        Router.push(`/${tabbar.link}`);
+                    }
+                } >
+                <div> { tabbar.text } </div> 
+                </div>
+            ))
+        } 
+        </ul>  
+        </div>
+    );
 }
