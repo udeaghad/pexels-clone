13269500a91ffff602f1a5f1cb0fc7b7979e707d
kingsley achumie <kingsonly13c@gmail.com>
2023-02-03 12:22:41 +0100

    wip

--- Components/Tabmenu/Tabmenu.js
+++ Components/Tabmenu/Tabmenu.js
@@ -7,6 +7,7 @@ import Router from "next/router";
  * Tabmenu is a component used to manage the tab menu which aids in navigating to different pages.
  * @function
  * @param {object}  props - stores the value of the tab menu list.
+ * @todo expext to check if the tab are mobile phone friendly.
  * @return {HTMLElement} 
  */
 export default function Tabmenu(props) {
@@ -20,10 +21,10 @@ export default function Tabmenu(props) {
 
     return (
          <div data-testid = "tabmenutext">
-        <ul className = "flex flex-row gap-4 text-slate-600 pt-6 " > 
+        <ul className = "flex flex-row gap-4 text-slate-600" > 
         {
             tablist.map((tabbar, index) => ( 
-                <div  className={currentRoute == tabbar.link ? "bg-black text-white h-12 flex justify-center items-center p-4 rounded-3xl cursor-pointer" : "bg-black h-12 flex justify-center items-center p-4 rounded-3xl cursor-pointer"}
+                <div  className={currentRoute == tabbar.link ? "bg-black  text-white h-[48px] flex justify-center items-center p-4 rounded-3xl cursor-pointer text-[16px] px-px-[20px] py-px-[0px]" : "h-12 flex justify-center items-center p-4 rounded-3xl cursor-pointer"}
                 key = { tabbar.id }
                 onClick = {
                     () => {
@@ -31,7 +32,7 @@ export default function Tabmenu(props) {
                         
                     }
                 } >
-                <div  className={currentRoute == tabbar.link ? "active" : "inactive"}> { tabbar.text } </div> 
+                <div  className={currentRoute == tabbar.link ? "isactive" : "isinactive"}> { tabbar.text } </div> 
                 </div>
             ))
         } 
