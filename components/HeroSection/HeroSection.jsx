b3ba2d8280dd1a97bd82c34c7dbf2711bee28641
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-09-26 19:48:20 -0400

    Create social media icons for mobile modal menu

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -11,47 +11,63 @@ import { RiFacebookFill } from "react-icons/ri";
 import { FaTwitter } from "react-icons/fa";
 import {BsInstagram} from "react-icons/bs";
 import { FaPinterestP } from "react-icons/fa";
-import { FaSquareYoutube } from "react-icons/fa";
+import { FaYoutubeSquare } from "react-icons/fa";
+import { AiOutlineYoutube } from "react-icons/ai";
+
+const navItems1 = [
+  {name: "Home", link: "#"},
+  {name: "Discover Photos", link: "#"},
+  {name: "Popular Searches", link: "#"},
+  {name: "Free Videos", link: "#"},
+  {name: "Challenges", link: "#"},
+  {name: "Leaderboard", link: "#"},
+  {name: "Pexels Blog", link: "#"},
+]
+
+const navItems2 = [
+{name: "Login", link: "#"},
+{name: "Join", link: "#"},
+{name: "Change Languge", link: "#"},
+{name: "Licence", link: "#"},
+]
+
+const navItems3 = [
+{name: "Apps & Plugins", link: "#"},
+{name: "FAQ", link: "#"},
+{name: "About Us", link: "#"},
+{name: "Imprint & Terms", link: "#"},
+]
+
+const socialMediaItems = [
+{name: "Facebook", link: "#", icon: RiFacebookFill},
+{name: "Twitter", link: "#", icon: FaTwitter},
+{name: "Instagram", link: "#", icon: BsInstagram},
+{name: "Pinterest", link: "#", icon: FaPinterestP},
+{name: "Youtube", link: "#", icon: AiOutlineYoutube},
+
+
+]
+
+const RenderSocialMedia = () => {
+  return (
+    <div className="flex justify-between items-center mt-7 mx-5 border-b pb-7 border-gray-800">
+      {socialMediaItems.map((item, i) => (
+        <div key={i} className="text-white text-xl font-medium cursor-pointer hover:text-gray-500">
+          <item.icon className="text-3xl"/>
+        </div>
+      ))}
+    </div>
+  )
+}
 
 const HeroSection = () => {
   const [ open, setOpen ] = useState(false);
 
-  const navItems1 = [
-    {name: "Home", link: "#"},
-    {name: "Discover Photos", link: "#"},
-    {name: "Popular Searches", link: "#"},
-    {name: "Free Videos", link: "#"},
-    {name: "Challenges", link: "#"},
-    {name: "Leaderboard", link: "#"},
-    {name: "Pexels Blog", link: "#"},
-  ]
-
-const navItems2 = [
-  {name: "Login", link: "#"},
-  {name: "Join", link: "#"},
-  {name: "Change Languge", link: "#"},
-  {name: "Licence", link: "#"},
-]
-
-const navItems3 = [
-  {name: "Apps & Plugins", link: "#"},
-  {name: "FAQ", link: "#"},
-  {name: "About Us", link: "#"},
-  {name: "Imprint & Terms", link: "#"},
-]
-
-const socialMediaItems = [
-  {name: "Facebook", link: "#", icon: <RiFacebookFill className="text-white text-2xl"/>},
-  {name: "Twitter", link: "#", icon: <FaTwitter className="text-white text-2xl"/>},
-  {name: "Instagram", link: "#", icon: <BsInstagram className="text-white text-2xl"/>},
-  {name: "Pinterest", link: "#", icon: <FaPinterestP className="text-white text-2xl"/>},
-  {name: "Youtube", link: "#", icon: <FaSquareYoutube className="text-white text-2xl"/>},
-]
 
 
   return (
     <div className="bg-[url('https://images.pexels.com/photos/17928278/pexels-photo-17928278.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1500')] w-full bg-center bg-cover bg-no-repeat relative"> 
-      <div className="flex justify-between items-center gap-3 p-4">
+      <div className="flex justify-between items-center gap-3 p-4 w-full">
         <div className="z-10">
           <Image src="/images/logo.png" alt="hero" width={50} height={50} className="rounded-md"/>
         </div>
@@ -114,14 +130,18 @@ const socialMediaItems = [
           ))}
         </div>
 
-        <div className="flex flex-col justify-start items-start gap-5 mt-10 mx-5 border-b pb-10 border-gray-800">
+        <div className="flex flex-col justify-start items-start gap-5 mt-10 mx-5 pb-10">
           {navItems3.map((item, i) => (
             <div key={i} className="text-white text-xl font-medium cursor-pointer hover:text-gray-500">
               <span>{item.name}</span>
             </div>
           ))}
-        </div>
-        
+        </div> 
+
+        <div className="border-t w-full border-gray-800"/>
+
+       
+        <RenderSocialMedia />     
         
       </div>    
     </div>
