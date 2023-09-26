320a48151d951baf648409d3ac62cc02c87e9900
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-09-26 18:06:33 -0400

    Create te first menu items

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -7,12 +7,51 @@ import { HiOutlineSearch } from "react-icons/hi";
 import { BiImageAlt } from "react-icons/bi";
 import { MdOutlineKeyboardArrowUp } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
+import { RiFacebookFill } from "react-icons/ri";
+import { FaTwitter } from "react-icons/fa";
+import {BsInstagram} from "react-icons/bs";
+import { FaPinterestP } from "react-icons/fa";
+import { FaSquareYoutube } from "react-icons/fa";
 
 const HeroSection = () => {
   const [ open, setOpen ] = useState(false);
+
+  const navItems1 = [
+    {name: "Home", link: "#"},
+    {name: "Discover Photos", link: "#"},
+    {name: "Popular Searches", link: "#"},
+    {name: "Free Videos", link: "#"},
+    {name: "Challenges", link: "#"},
+    {name: "Leaderboard", link: "#"},
+    {name: "Pexels Blog", link: "#"},
+  ]
+
+const navItems2 = [
+  {name: "Login", link: "#"},
+  {name: "Join", link: "#"},
+  {name: "Change Languge", link: "#"},
+  {name: "Licence", link: "#"},
+]
+
+const navItems3 = [
+  {name: "Apps & Plugins", link: "#"},
+  {name: "FAQ", link: "#"},
+  {name: "About Us", link: "#"},
+  {name: "Imprint & Terms", link: "#"},
+]
+
+const socialMediaItems = [
+  {name: "Facebook", link: "#", icon: <RiFacebookFill className="text-white text-2xl"/>},
+  {name: "Twitter", link: "#", icon: <FaTwitter className="text-white text-2xl"/>},
+  {name: "Instagram", link: "#", icon: <BsInstagram className="text-white text-2xl"/>},
+  {name: "Pinterest", link: "#", icon: <FaPinterestP className="text-white text-2xl"/>},
+  {name: "Youtube", link: "#", icon: <FaSquareYoutube className="text-white text-2xl"/>},
+]
+
+
   return (
-    <div className=" p-5 bg-[url('https://images.pexels.com/photos/17928278/pexels-photo-17928278.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1500')] w-full bg-center bg-cover bg-no-repeat relative"> 
-      <div className="flex justify-between items-center gap-3">
+    <div className="bg-[url('https://images.pexels.com/photos/17928278/pexels-photo-17928278.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1500')] w-full bg-center bg-cover bg-no-repeat relative"> 
+      <div className="flex justify-between items-center gap-3 p-4">
         <div className="z-10">
           <Image src="/images/logo.png" alt="hero" width={50} height={50} className="rounded-md"/>
         </div>
@@ -56,6 +95,16 @@ const HeroSection = () => {
       </div>  
 
       <div className="bg-black w-full h-[100vh] fixed top-0 bottom-0 left-0 right-0 z-5" style={{display: open ? "block" : "none"}}>
+
+        <div className="border-t w-full mt-20 border-slate-500"/>
+
+        <div className="flex flex-col justify-start items-start gap-5 mt-10 ml-5 border-b pb-10">
+          {navItems1.map((item, i) => (
+            <div key={i} className="text-white text-xl font-medium cursor-pointer hover:text-gray-500">
+              <span>{item.name}</span>
+            </div>
+          ))}
+        </div>
         
         
       </div>    
