3314d653c4ac975b2bda7c28477f697562ff5b51
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-09-26 17:33:57 -0400

    Implement modal opening for Navbar

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -17,38 +17,45 @@ const HeroSection = () => {
           <Image src="/images/logo.png" alt="hero" width={50} height={50} className="rounded-md"/>
         </div>
 
-        <div className="flex justify-center items-center gap-2 hidden">
-          <div className="text-lg font-sans px-5 py-2 rounded-md bg-white w-fit border border-[#dfdfe0]">
-            <span>Join</span>
-          </div>
+        { !open && (
 
-          <div className="p-1">
-            <HiOutlineMenu className="text-white text-2xl font-bold"/>
-          </div>
-        </div>
+          <div className="flex justify-center items-center gap-2">
+            <div className="text-lg font-sans px-5 py-2 rounded-md bg-white w-fit border border-[#dfdfe0]">
+              <span>Join</span>
+            </div>
 
-        <div className="flex w-full justify-between items-center z-10 gap-2"> 
-          <div className="bg-white flex justify-between items-center w-full rounded-md px-2" >
-            <div className="flex justify-center items-center">
-              <BiImageAlt className="text-2xl text-gray-500"/>
-              <MdOutlineKeyboardArrowUp className="text-xl text-gray-500"/>
-            </div>
-            <div>
-              <input type="text"  placeholder="Search for free photos" className="py-2 text-lg font-medium outline-2 w-full" />
-            </div>
-            <div>
-              <HiOutlineSearch className="text-xl text-gray-400"/>
+            <div className="p-1" onClick={() => setOpen(true)}>
+              <HiOutlineMenu className="text-white text-2xl font-bold"/>
             </div>
           </div>
+        )}
 
-          <div>
-            <IoClose className="text-white text-2xl font-bold"/>
+        { open && (
+          <div className="flex w-full justify-between items-center z-10 gap-2"> 
+            <div className="bg-white flex justify-between items-center w-full rounded-md px-2 py-1 gap-1" >
+              <div className="flex justify-center items-center">
+                <BiImageAlt className="text-2xl text-gray-500"/>
+                <MdOutlineKeyboardArrowUp className="text-xl text-gray-500"/>
+              </div>
+              <div >
+                <input type="text"  placeholder="Search for free photos" className="p-1 text-base font-medium outline-2 w-full border-l" />
+              </div>
+              <div>
+                <HiOutlineSearch className="text-xl text-gray-400"/>
+              </div>
+            </div>
+
+            <div onClick={() => setOpen(false)}>
+              <IoClose className="text-white text-2xl font-bold"/>
+            </div>
+
           </div>
+        )}
 
-        </div>
+
       </div>  
 
-      <div className="bg-black w-full h-[100vh] fixed top-0 bottom-0 left-0 right-0 z-5">
+      <div className="bg-black w-full h-[100vh] fixed top-0 bottom-0 left-0 right-0 z-5" style={{display: open ? "block" : "none"}}>
         
         
       </div>    
