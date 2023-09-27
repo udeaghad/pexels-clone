79c9280942c4d726dd628484a3006717ddd50ec2
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-09-27 10:43:12 +0100

    create trending text

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 import { HiOutlineMenu} from "react-icons/hi";
 import { HiOutlineSearch } from "react-icons/hi";
 import { BiImageAlt } from "react-icons/bi";
-import { MdOutlineKeyboardArrowUp } from "react-icons/md";
+import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
 import { RiFacebookFill } from "react-icons/ri";
 import { FaTwitter } from "react-icons/fa";
 import {BsInstagram} from "react-icons/bs";
 import { FaPinterestP } from "react-icons/fa";
-import { FaYoutubeSquare } from "react-icons/fa";
+import { HiDotsCircleHorizontal } from "react-icons/hi";
 import { AiOutlineYoutube } from "react-icons/ai";
 
 const navItems1 = [
@@ -90,7 +90,7 @@ const HeroSection = () => {
             <div className="bg-white flex justify-between items-center w-full rounded-md px-2 py-1 gap-1" >
               <div className="flex justify-center items-center">
                 <BiImageAlt className="text-2xl text-gray-500"/>
-                <MdOutlineKeyboardArrowUp className="text-xl text-gray-500"/>
+                <MdOutlineKeyboardArrowDown className="text-xl text-gray-500"/>
               </div>
               <div >
                 <input type="text"  placeholder="Search for free photos" className="p-1 text-base font-medium outline-2 w-full border-l" />
@@ -114,19 +114,28 @@ const HeroSection = () => {
         <p className="text-white text-4xl font-bold leading-tight">
           The best free stock photos, royal free images & videos shared by creators.
         </p>
-      </div> 
+      </div>       
 
       <div className="bg-white flex justify-between items-center rounded-md px-2 py-1 gap-1 mx-5" >
         <div className="flex justify-center items-center bg-gray-100 rounded-md border border-gray-400 p-2">
           <BiImageAlt className="text-3xl text-gray-500"/>
-          <MdOutlineKeyboardArrowUp className="text-2xl text-gray-500"/>
+          <MdOutlineKeyboardArrowDown className="text-2xl text-gray-500"/>
         </div>
         <div >
           <input type="text"  placeholder="Search for free photos" className="px-2 py-3 text-xl font-medium outline-2 w-full" />
         </div>
         <div>
-          <HiOutlineSearch className="text-xl text-gray-400"/>
+          <HiOutlineSearch className="text-2xl text-gray-400"/>
         </div>
+      </div>
+
+      <div className="flex justify-baseline items-center text-xl text-white font-bold mx-5 my-10 overflow-hidden gap-1">
+        <span className="text-gray-300"> 
+          Trending: 
+        </span> 
+        <span className="text-white w-full whitespace-nowrap overflow-hidden">smoke, time, team, pattern, data {' '} </span>
+        <HiDotsCircleHorizontal className="text-gray-300 text-5xl"/>
+          
       </div>
 
       <div className="bg-black w-full h-[100vh] absolute top-0 bottom-0 left-0 right-0 z-5 overflow-auto" style={{display: open ? "block" : "none"}}>
