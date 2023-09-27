b612d75532082b14535cb4197e416802911ce549
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-09-27 16:00:10 +0100

    Implement scroll functionality for navitems

--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -1,25 +1,14 @@
+import { useInView } from 'react-intersection-observer';
 import Link from "next/link";
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight, MdOutlineKeyboardArrowLeft } from "react-icons/md";
-const navitems = [
-  {
-    name: 'Home',
-    link: '#'
-  },
-  {
-    name: 'Videos',
-    link: '#'
-  },
-  {
-    name: 'Leaderboard',
-    link: '#'
-  },
-  {
-    name: 'Challenges',
-    link: '#'
-  },
-];
+
 
 const Navbar = () => {
+  const { ref: start, inView: startView} = useInView();
+  const { ref: end, inView: endView} = useInView();
+
+console.log({ startView, endView})
+
   const slideLeft = () => {
     const slider = document.getElementById('slider');
     slider.scrollLeft = slider.scrollLeft - 100;
@@ -32,19 +21,41 @@ const Navbar = () => {
 
   return (
    <nav className="relative flex justify-between items-center w-full mt-5 ml-0">
-    <div className="absolute bg-gradient-to-r w-16 h-16 from-white flex justify-start items-center">
+    <div className="absolute bg-gradient-to-r w-12 h-16 from-white justify-start items-center" style={{display: startView && !endView ? "none" : startView && !endView ? "flex" : "flex"}}>
       <MdKeyboardArrowLeft size={30} className="text-gray-900 opacity-50 bg-[inherit] bg-blend-lighten" onClick={slideLeft}/>
 
     </div>
-    <div id="slider" className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide px-5">
-      {navitems.map((item) => (
-        <Link href={item.link}>
-          <span className="py-3 px-5 text-lg rounded-full hover:text-slate-200 hover:bg-black cursor-pointer inline-block font-semibold text-gray-500">{item.name}</span>
-        </Link>
 
-      ))}
+    <div id="slider" className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide px-5">
+      <span className="py-3 px-5 text-lg rounded-full hover:text-slate-200 hover:bg-black cursor-pointer inline-block font-semibold text-gray-500">
+        <Link href="#"ref={start}>
+          Home
+        </Link>
+      </span>
+      
+
+      <span className="py-3 px-5 text-lg rounded-full hover:text-slate-200 hover:bg-black cursor-pointer inline-block font-semibold text-gray-500">
+        <Link href="#">
+          Videos
+        </Link>
+      </span>
+
+      <span className="py-3 px-5 text-lg rounded-full hover:text-slate-200 hover:bg-black cursor-pointer inline-block font-semibold text-gray-500">
+        <Link href="#">
+          Leaderboard
+        </Link>
+      </span>
+
+      <span className="py-3 px-5 text-lg rounded-full hover:text-slate-200 hover:bg-black cursor-pointer inline-block font-semibold text-gray-500">
+        <Link href="#" ref={end}>
+          Challenges
+        </Link>
+      </span>
     </div>
-    <div className="absolute bg-gradient-to-l w-16 h-16 from-white flex justify-end items-center right-0">
+     
+
+    
+    <div className="absolute bg-gradient-to-l w-12 h-16 from-white justify-end items-center right-0" style={{display: !startView && endView ? "none" : startView && endView ? "flex": "flex"}}>
       <MdKeyboardArrowRight size={30} className="text-gray-900 opacity-50 bg-[inherit] bg-blend-lighten" onClick={slideRight}/>
 
     </div>
