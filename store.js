cd7b58e788353873076a7ed63abbbb89871d6408
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-09-30 11:02:24 +0100

    Update store with video

--- store.js
+++ store.js
@@ -2,7 +2,9 @@ import { create } from 'zustand'
 
 const store = (set) => ({
   photos: [],
+  videos: [],
   addPhotos: (photoList) => set((state) => ({ photos: [...state.photos, ...photoList] })),
+  addVideos: (videoList) => set((state) => ({ videos: [...state.videos, ...videoList] })),
 });
 
 export const useStore = create(store);
