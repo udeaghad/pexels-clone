153720db8b5ea93a8293b44809d4136fd14f09b2
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-10-02 14:59:43 +0100

    Create dropdown for others on desktop navbar component

--- components/MobileNavBar/MobileNavBar.jsx
+++ components/MobileNavBar/MobileNavBar.jsx
@@ -6,6 +6,32 @@ import { MdOutlineKeyboardArrowDown } from "react-icons/md";
 import { IoClose } from "react-icons/io5";
 import { useRef } from "react";
 
+import { RiFacebookFill } from "react-icons/ri";
+import { FaTwitter } from "react-icons/fa";
+import {BsInstagram} from "react-icons/bs";
+import { FaPinterestP } from "react-icons/fa";
+import { AiOutlineYoutube } from "react-icons/ai";
+
+const socialMediaItems = [
+  {name: "Facebook", link: "#", icon: RiFacebookFill},
+  {name: "Twitter", link: "#", icon: FaTwitter},
+  {name: "Instagram", link: "#", icon: BsInstagram},
+  {name: "Pinterest", link: "#", icon: FaPinterestP},
+  {name: "Youtube", link: "#", icon: AiOutlineYoutube},  
+]
+
+const RenderSocialMedia = () => {
+  return (
+    <div className="flex justify-between items-center my-3 mx-5 gap-5">
+      {socialMediaItems.map((item, i) => (
+        <div key={i} className="font-medium cursor-pointer hover:text-gray-500">
+          <item.icon className="text-xl"/>
+        </div>
+      ))}
+    </div>
+  )
+}
+
 const MobileNavBar = ({open, setOpen, showHamburger}) => {
 
   const exploreOptions = useRef(null)
@@ -87,9 +113,57 @@ const MobileNavBar = ({open, setOpen, showHamburger}) => {
               <span>Upload</span>
             </div>
 
-            <div className="flex justify-center items-center gap-1 hover:text-gray-300">
-              <HiDotsHorizontal size={25} className="text-white hover:text-gray-300"/>
+            <div className="relative">
+              <div className=" mt-5 pb-5 flex justify-center items-center gap-1">
+                <HiDotsHorizontal size={25} className="text-white hover:text-gray-300"/>
+              </div>
+
+              <div className="absolute">
+              <div  className="w-0 h-0 border-l-[2rem] border-r-[2rem] border-l-transparent border-r-transparent border-b-[2rem] border-b-white absolute -top-2 -left-6 z-10"/>
+                
+                <div className="flex flex-col bg-white text-gray-600 font-normal pt-3 pb-3 rounded-xl gap-1 absolute text-sm -left-44 border">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                    <span>Login</span>
+                  </div>
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                    <span>Join</span>
+                  </div>
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                    <span>Change Language</span>
+                  </div>
+
+                  <div className="border-b border-gray-100"/>
+
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                    <span>Image & Video API</span>
+                  </div>
+
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                    <span>Apps & Plugins</span>
+                  </div>
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                    <span>FAQ</span>
+                  </div>
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                    <span>
+                      Partnership
+                    </span>
+                  </div>
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                    <span>
+                      Imprint & Terms
+                    </span>
+                  </div>
+
+                  <div className="border-b border-gray-100"/>
+
+                  <RenderSocialMedia />
+
+                </div>
+                
+              </div>
             </div>
+
               
 
           </div>
