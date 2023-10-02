f6db491df84e09b159003bd89cdee55f843155a8
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-10-02 15:20:42 +0100

    Update Styling of othersBtn on navBar

--- components/MobileNavBar/MobileNavBar.jsx
+++ components/MobileNavBar/MobileNavBar.jsx
@@ -25,7 +25,7 @@ const RenderSocialMedia = () => {
     <div className="flex justify-between items-center my-3 mx-5 gap-5">
       {socialMediaItems.map((item, i) => (
         <div key={i} className="font-medium cursor-pointer hover:text-gray-500">
-          <item.icon className="text-xl"/>
+          <item.icon className="text-xl text-gray-400 hover:text-gray-600"/>
         </div>
       ))}
     </div>
@@ -81,7 +81,7 @@ const MobileNavBar = ({open, setOpen, showHamburger}) => {
         <div className="flex justify-center items-center gap-2 md:gap-5">
           <div className="hidden md:flex text-white font-bold justify-center items-center gap-5">
             <div className="relative">
-              <div className="flex justify-center items-center mt-3 pb-3 gap-1" onMouseEnter={ handleOpenExploreBtn} onMouseLeave={handleCloseExploreBtn}>
+              <div className="flex justify-center items-center mt-3 pb-3 gap-1 cursor-pointer" onMouseEnter={ handleOpenExploreBtn} onMouseLeave={handleCloseExploreBtn}>
                 <div ref={exploreBtn} className="flex">
                   <span>Explore</span>
                   <div ref={arrowBtn} className="text-white">
@@ -94,22 +94,22 @@ const MobileNavBar = ({open, setOpen, showHamburger}) => {
               <div className="absolute hidden" ref={exploreOptions} onMouseEnter={handleOpenExploreBtn} onMouseLeave={handleCloseExploreBtn}>
                 <div  className="w-0 h-0 border-l-[2rem] border-r-[2rem] border-l-transparent border-r-transparent border-b-[2rem] border-b-white absolute -top-2 left-10 z-10"/>
                 <div className="flex flex-col bg-white text-gray-600 font-normal pt-3 pb-3 rounded-xl gap-1 absolute text-sm w-40 -left-14 border"> 
-                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20 cursor-pointer">
                     <span>Discover Photos</span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1">
+                  <div className="hover:bg-gray-100 px-3 py-1 cursor-pointer">
                     <span>Popular Searches</span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1">
+                  <div className="hover:bg-gray-100 px-3 py-1 cursor-pointer">
                     <span>Leaderboard</span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1">
+                  <div className="hover:bg-gray-100 px-3 py-1 cursor-pointer">
                     <span>Challenges</span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1">
+                  <div className="hover:bg-gray-100 px-3 py-1 cursor-pointer">
                     <span>Free Videos</span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1">
+                  <div className="hover:bg-gray-100 px-3 py-1 cursor-pointer">
                     <span>Pexels Blog</span>
                   </div>
                 </div>
@@ -118,16 +118,16 @@ const MobileNavBar = ({open, setOpen, showHamburger}) => {
 
             </div>
 
-            <div className="flex justify-center items-center gap-1 hover:text-gray-300">
+            <div className="flex justify-center items-center gap-1 hover:text-gray-300 cursor-pointer">
               <span>Licence</span>
             </div>
 
-            <div className="flex justify-center items-center gap-1 hover:text-gray-300">
+            <div className="flex justify-center items-center gap-1 hover:text-gray-300 cursor-pointer">
               <span>Upload</span>
             </div>
 
             <div className="relative">
-              <div className=" mt-5 pb-5 flex justify-center items-center gap-1" onMouseEnter={handleOpenOthersBtn} onMouseLeave={handleCloseOthersBtn}>
+              <div className=" mt-5 pb-5 flex justify-center items-center gap-1 cursor-pointer" onMouseEnter={handleOpenOthersBtn} onMouseLeave={handleCloseOthersBtn}>
                 <HiDotsHorizontal size={25} className="text-white hover:text-gray-300"/>
               </div>
 
@@ -135,34 +135,34 @@ const MobileNavBar = ({open, setOpen, showHamburger}) => {
               <div  className="w-0 h-0 border-l-[2rem] border-r-[2rem] border-l-transparent border-r-transparent border-b-[2rem] border-b-white absolute -top-2 -left-6 z-10"/>
                 
                 <div className="flex flex-col bg-white text-gray-600 font-normal pt-3 pb-3 rounded-xl gap-1 absolute text-sm -left-44 border">
-                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20 cursor-pointer">
                     <span>Login</span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20 cursor-pointer">
                     <span>Join</span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20 cursor-pointer">
                     <span>Change Language</span>
                   </div>
 
                   <div className="border-b border-gray-100"/>
 
-                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20 cursor-pointer">
                     <span>Image & Video API</span>
                   </div>
 
-                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20 cursor-pointer">
                     <span>Apps & Plugins</span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20 cursor-pointer">
                     <span>FAQ</span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20 cursor-pointer">
                     <span>
                       Partnership
                     </span>
                   </div>
-                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20">
+                  <div className="hover:bg-gray-100 px-3 py-1 relative z-20 cursor-pointer">
                     <span>
                       Imprint & Terms
                     </span>
@@ -182,7 +182,7 @@ const MobileNavBar = ({open, setOpen, showHamburger}) => {
           </div>
           
 
-          <div className="text-lg font-sans px-5 py-2 rounded-md bg-white w-fit border border-[#dfdfe0]">
+          <div className="text-lg font-sans px-5 py-2 rounded-md bg-white w-fit border border-[#dfdfe0] outline-2 cursor-pointer">
             <span>Join</span>
           </div>
 
