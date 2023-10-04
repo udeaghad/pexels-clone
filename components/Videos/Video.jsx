1066243318d06be9d41a14cfdb7d00a8b4009d7b
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-10-04 15:25:20 +0100

    Update styling on video component

--- components/Videos/Video.jsx
+++ components/Videos/Video.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import ReactLoading from 'react-loading';
 import { FaYoutube } from "react-icons/fa";
 import Image from "next/image";
+import { useRef } from "react";
 
 const Videos = ({videos, setInView, handleOpenModal}) => {
 
@@ -14,19 +15,53 @@ const Videos = ({videos, setInView, handleOpenModal}) => {
       setInView(inView)    
   }, [inView])
 
+ useEffect(() => {
+   console.log(videos)
+  }, [videos])
+
+  const videoIconRef = useRef();
+
+  const handlePlayVideo = (i) => {
+    const video = document.getElementById(`video-${i}`);
+    const videoIcon = document.getElementById(`videoIcon-${i}`);
+    videoIcon.classList.add("hidden");
+    video.play();    
+  }
+
+  const HandleStopVideo = (i) => {
+    const video = document.getElementById(`video-${i}`);
+    const videoIcon = document.getElementById(`videoIcon-${i}`);
+    video.pause();
+    videoIcon.classList.remove("hidden");
+  }
+
    return (
     <div className="px-2 absolute -z-20 w-full mt-32">
 
       <ResponsiveMasonry
-        columnsCountBreakPoints={{300: 2, 750: 2, 900: 3}}
-        
+        columnsCountBreakPoints={{300: 2, 900: 3}}
       >
         <Masonry >
           {videos.map((video, i) => (
             <div key={i} className="relative p-2" onClick={() => handleOpenModal(video)}>
-              <Image src={video.image} alt={ video.user.name} width={100} height={100} className="w-full h-full object-cover"/>
-              <FaYoutube size={30} className="absolute top-5 left-5 text-white cursor-pointer hover:text-gray-500"/>
-              <FiDownload size={25} className="absolute bottom-5 right-5 text-white cursor-pointer hover:text-gray-500"/>
+              <div className="mt-5 w-full flex">
+                <video 
+                  src={video.video_files[0].link} loop type={video.video_files[0].file_type} 
+                  className="w-full h-full object-cover" 
+                  id={`video-${i}`}
+                  onMouseEnter={() => handlePlayVideo(i)}
+                  onMouseLeave={() => HandleStopVideo(i)}
+                  />
+                
+              </div>
+              <div className="absolute bottom-5 right-5 cursor-pointer text-white font-medium hover:bg-gray-100 hover:opacity-80 p-2 rounded-lg hover:text-black sm:hidden" >
+                <FiDownload size={20} />
+              </div>
+              <div className="absolute top-7 left-5 cursor-pointer text-white font-medium sm:hidden" id={`videoIcon-${i}`}>
+                <FaYoutube size={40} />
+              </div>
+
+              
 
             </div>
           ))}
