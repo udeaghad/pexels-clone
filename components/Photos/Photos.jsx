e265f174243ce28847ddd97e3ba1d243b350e400
Emmanuel Chinonye Nnajiofor <emma.nnajichi@gmail.com>
2023-10-05 23:10:26 +0100

    minor changes

--- components/Photos/Photos.jsx
+++ components/Photos/Photos.jsx
@@ -57,13 +57,12 @@ const Photos = ({photos, setInView, handleOpenModal}) => {
 
       <ResponsiveMasonry
         columnsCountBreakPoints={{300: 2, 900: 3}}
-        
       >
         <Masonry >
           
           {photoToRender.length > 0 && photoToRender.map((photo, i) => (
-            <div key={i} className="relative z-5 p-2" onClick={() => handleOpenModal(photo)} onMouseEnter={() => hanldeShowIcons(i)} onMouseLeave={() => handleDisappearIcon(i)}>
-              <Image src={photo.src.medium} alt={photo.photographer} width={100} height={100} className="w-full h-full object-cover"/>
+            <div key={photo?.src || i} className="relative z-5 p-2" onClick={() => handleOpenModal(photo)} onMouseEnter={() => hanldeShowIcons(i)} onMouseLeave={() => handleDisappearIcon(i)}>
+              <Image src={photo?.src?.original} alt={photo?.photographer} width={photo?.width|| 100} height={photo?.height || 100} className="w-full h-full object-cover"/>
               <div className="absolute bottom-5 right-5 cursor-pointer text-white font-medium hover:bg-gray-100 hover:opacity-80 p-2 rounded-lg hover:text-black sm:hidden">
                 <FiDownload size={20} />
               </div>
@@ -102,7 +101,7 @@ const Photos = ({photos, setInView, handleOpenModal}) => {
       <div ref={ref} className="h-1 bg-white m-5"/>
 
       <div className="flex justify-center items-center">
-        <ReactLoading type="balls" color="gray" height={50} width={50} />
+        <ReactLoading type="balls" color="gray" height={200} width={200} />
       </div>
     </div>
   )
