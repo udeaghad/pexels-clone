e9c5f510050646cebcebaac638c0d07fb528b265
Dozie Udeagha <dozie.udeagha@gmail.com>
2023-10-19 11:46:32 +0100

    Create banner text on onboarding page

--- pages/onboarding/index.js
+++ pages/onboarding/index.js
@@ -5,6 +5,12 @@ const Home = () => {
   return (
     <div>
       <OnBoardingNavBar />
+
+      <div className='flex justify-center items-center w-full px-10 py-3'>
+        <h1 className='text-[40px] text-center font-semibold text-gray-800'>
+          What do you mainly want to do?
+        </h1>
+      </div>
     </div>
   )
 }
